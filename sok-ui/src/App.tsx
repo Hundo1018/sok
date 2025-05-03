007cd0b328ec9c91bc6c9edebfb77db207aa92ed
Hundo1018 <f74169@gmail.com>
2025-05-03 23:26:25 +0800

    build sok-ui to load wasm

---
+++ sok-ui/src/App.tsx
@@ -0,0 +1,16 @@
+import  { useEffect } from 'react'
+import sok from './pkg/sok'
+import './App.css'
+
+function App() {
+  useEffect(() => { 
+    sok()
+  }, [])
+  return (
+    <div>
+      <canvas id="wasm-sok" width={800} height={600}></canvas>
+    </div>
+  );
+}
+
+export default App
